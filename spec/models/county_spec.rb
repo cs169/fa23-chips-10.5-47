d41181ea1f19722f342dbe933b051badfc17d6d0
Henry Chiu <henrychiu@berkeley.edu>
2023-12-07 04:16:49 +0000

    Fix

--- spec/models/county_spec.rb
+++ spec/models/county_spec.rb
@@ -2,21 +2,21 @@
 
 require 'rails_helper'
 
-describe County, type: :model do
+describe County do
   describe '#std_fips_code' do
-    it 'returns a FIPS from a single digit' do
-      state = described_class.new(fips_code: 1)
-      expect(state.std_fips_code).to eq('001')
+    it 'returns a formatted FIPS if given a single digit' do
+      state = described_class.new(fips_code: 3)
+      expect(state.std_fips_code).to eq('003')
     end
 
-    it 'returns a FIPS from double digits' do
-      state = described_class.new(fips_code: 12)
-      expect(state.std_fips_code).to eq('012')
+    it 'returns a formatted FIPS if given double digits' do
+      state = described_class.new(fips_code: 32)
+      expect(state.std_fips_code).to eq('032')
     end
 
-    it 'returns a FIPS from triple digits' do
-      state = described_class.new(fips_code: 123)
-      expect(state.std_fips_code).to eq('123')
+    it 'returns a formatted FIPS if given triple digits' do
+      state = described_class.new(fips_code: 321)
+      expect(state.std_fips_code).to eq('321')
     end
   end
 end
