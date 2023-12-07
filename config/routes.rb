d108978895b5ebac0481a79daca0aab65f9c2f3f
Henry Chiu <henrychiu@berkeley.edu>
2023-12-07 01:52:29 +0000

    Add campaign finance search page

--- config/routes.rb
+++ config/routes.rb
@@ -40,4 +40,8 @@ Rails.application.routes.draw do
                                                                       via: [:delete]
     end
     get '/search/(:address)' => 'search#search', :as => 'search_representatives'
+
+    resources :campaign_finance, only: [:index]
+    get '/campaign_finance/search' => 'campaign_finance#search', :as => 'search_campaign_finance'
 end
+ 
